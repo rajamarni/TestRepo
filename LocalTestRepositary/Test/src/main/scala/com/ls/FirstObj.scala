6f7b4091809e4ac60dedadf4f1be425313f544b7
tarak <raja.marni@gmail.com>
2017-01-06 17:29:31 +0530

    HI

---
+++ LocalTestRepositary/Test/src/main/scala/com/ls/FirstObj.scala
@@ -0,0 +1,16 @@
+package main.scala.com.ls
+
+
+import java.io.File
+import org.apache.spark.sql._
+
+
+import org.apache.spark.SparkConf
+import org.apache.spark.SparkContext
+
+
+object FirstObj {
+  def main(Args: Array[String]){
+    println("FirstObject")
+  }
+}
